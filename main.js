f923d081bce18bb5e42b63b36f7f11d6ea55c464
iicervantes <iicervantes.career@gmail.com>
2016-08-29 15:58:21 -0400

    Update main.js

--- main.js
+++ main.js
@@ -44,5 +44,47 @@ var test = {
     ]
 };
 
-ReactDOM.render(<Exam details={test}/>, document.getElementById('exam'));
+var testDB = {
+    topic: "Database",
+    questions: [
+        {
+            id: "1",
+            ques:"What is sql injection?",
+            ans:"Hackers doing stuff",
+            weight: 5,
+            options:[
+                {text:"Hackers doing stuff"},
+                {text:"Inserting data in sql"},
+                {text:"Auto data manipulation from database"},
+                {text:"I don't know sql"}
+            ]
+        },
+        {
+            id: "2",
+            ques:"Difference between truncate and delete?",
+            ans:"You can't rollback with truncate.",
+            weight: 5,
+            options:[
+                {text:"The same"},
+                {text:"Truncate sounds cool"},
+                {text:"I like truncate"},
+                {text:"You can't rollback with truncate."}
+            ]
+        },
+        {
+            id: "5",
+            ques: "What is normalization?",
+            ans: "Reduce duplicate data",
+            weight: 1,
+            options: [
+                {text: "The process of becoming normal"},
+                {text: "Reduce duplicate data"},
+                {text: "3.5 is the best"},
+                {text: "Matrix process to civilize the world."}
+            ]
+        }
+    ]
+};
+
+ReactDOM.render(<Exam input={test}/>, document.getElementById('exam'));
 
